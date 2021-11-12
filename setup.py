5d154aa277ec10a95eed0cec6597e3b72e4b93d2
Tyler Clair <tyler.clair@gmail.com>
2021-11-12 08:27:53 -0700

    Incremented version to 0.2.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os import path
 
-__version__ = 0.2
+__version__ = "0.2.1"
 here = path.abspath(path.dirname(__file__))
 
 # get the dependencies and installs
